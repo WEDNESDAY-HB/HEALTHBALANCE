2f70b7891063f955c2c6d3d59826241f85dd0895
stella9921 <stella9921@naver.com>
2024-08-02 13:15:06 +0900

    0802 이현수 hb연결

---
+++ src/test/java/com/example/wednsday/WednsdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wednsday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WednsdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
